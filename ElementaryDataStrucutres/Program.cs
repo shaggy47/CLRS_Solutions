f9af56da8c62a842fdfea7fe9f5e31410524180b
Vaibhav <Vaibhav@GANGSTA47>
2017-10-11 11:47:31 +0530

    Queue added with basic operations.

--- ElementaryDataStrucutres/Program.cs
+++ ElementaryDataStrucutres/Program.cs
@@ -1,4 +1,5 @@
-﻿using ElementaryDataStrucutres.Stacks;
+﻿using ElementaryDataStrucutres.Queues;
+using ElementaryDataStrucutres.Stacks;
 using System;
 
 namespace ElementaryDataStrucutres
@@ -7,7 +8,18 @@ namespace ElementaryDataStrucutres
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(ExpressionEvaluator.Evaluate("( 10 + 2 ) + ( 4 + 5 )"));
+            Queue queue = new Queue();
+            queue.EnQueue(10);
+            queue.EnQueue(11);
+            queue.EnQueue(12);
+            queue.EnQueue(13);
+            queue.EnQueue(14);
+            queue.EnQueue(15);
+            Console.WriteLine("Head = {0}, Tail = {1}", queue.PeekHead(), queue.PeekTail());
+            while (!queue.IsEmpty())
+            {
+                Console.WriteLine(queue.DeQueue());
+            }
         }
     }
 }
---
+++ ElementaryDataStrucutres/Queues/Queue.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElementaryDataStrucutres.Queues
+{
+    class Queue
+    {
+        private int[] collection;
+        private int initialCap;
+        private int head;
+        private int tail;
+        private void IncreaseCapacity()
+        {
+            int[] temp = new int[initialCap * 2];
+            Array.Copy(collection, temp, collection.Length);
+        }
+
+        public Queue()
+        {
+            initialCap = 10;
+            collection = new int[initialCap];
+            head = 0;
+            tail = 0;
+        }
+
+        public int PeekHead()
+        {
+            return collection[head];
+        }
+
+        public int PeekTail()
+        {
+            return collection[tail-1];
+        }
+
+        public void EnQueue(int item)
+        {
+            if (tail == initialCap)
+                IncreaseCapacity();
+
+            collection[tail] = item;
+            tail++;
+        }
+
+        public int DeQueue()
+        {
+            if (head < tail)
+            {
+                int item = collection[head];
+                head++;
+                return item;
+            }
+            else
+                throw new Exception("Queue empty");
+        }
+
+        public bool IsEmpty()
+        {
+            if (head == tail)
+                return true;
+            else
+                return false;
+        }
+    }
+}
